ea8d36b250fa0d2e5d03f41d98956afa51f310f8
lukewatts2013-max <lukewatts2013@gmail.com>
2026-07-10 14:55:53 -0600

    Add models package with type definitions

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+"""Data models and type definitions for the league bot."""
+
+from models.types import Player, Assignment, Matchup, MatchupSnapshot, LeagueData
+
+__all__ = ["Player", "Assignment", "Matchup", "MatchupSnapshot", "LeagueData"]
